c3666cf3205fb104070ed194902eb6680b607af2
isadash <anchald2@illinois.edu>
2018-12-05 14:22:33 -0600

    try1

--- app/src/main/java/com/example/isadash/uiucfit/meals.java
+++ app/src/main/java/com/example/isadash/uiucfit/meals.java
@@ -2,12 +2,29 @@ package com.example.isadash.uiucfit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class meals extends AppCompatActivity {
+
+    private EditText food;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meals);
+
+        food = (EditText) findViewById(R.id.editFood);
+
+        Button btnEnter = (Button) findViewById(R.id.buttonEnter);
+        btnEnter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String h = food.getText().toString();
+                Toast.makeText(meals.this, "food entered successfully", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/isadash/uiucfit/profile.java
+++ app/src/main/java/com/example/isadash/uiucfit/profile.java
@@ -11,21 +11,39 @@ import android.widget.Toast;
 
 public class profile extends AppCompatActivity {
 
-    private EditText height, weight;
+    private EditText height, weight, calorie;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-        height = (EditText) findViewById(R.id.txtUserName);
+        height = (EditText) findViewById(R.id.editHeight);
+        weight = (EditText)  findViewById(R.id.editWeight);
+        calorie = (EditText)  findViewById(R.id.editCalorie);
 
-        Button btnLogin = (Button) findViewById(R.id.btnLogin);
-        btnLogin.setOnClickListener(new View.OnClickListener() {
+        Button btnHeight = (Button) findViewById(R.id.buttonHeight);
+        Button btnWeight = (Button) findViewById(R.id.buttonWeight);
+        Button btnCalorie = (Button) findViewById(R.id.buttonCalorie);
+        btnHeight.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String h = height.getText().toString();
-                TextView enteredHeight = (TextView) findViewById(R.id.newHeight);
+                Toast.makeText(profile.this, "Height updated successfully", Toast.LENGTH_LONG).show();
+            }
+        });
+        btnWeight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String w = weight.getText().toString();
+                Toast.makeText(profile.this, "Weight updated successfully", Toast.LENGTH_LONG).show();
+            }
+        });
+        btnCalorie.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String c = calorie.getText().toString();
+                Toast.makeText(profile.this, "Calorie goal updated successfully", Toast.LENGTH_LONG).show();
             }
         });
 
