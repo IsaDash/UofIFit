c1a02dc6947a888ce2dde21e87a8e30c5a36ee5b
Isa Dash <anchald2@illinois.edu>
2018-12-07 18:17:38 -0600

    Update camera.java

--- app/src/main/java/com/example/isadash/uiucfit/camera.java
+++ app/src/main/java/com/example/isadash/uiucfit/camera.java
@@ -1,13 +1,49 @@
 package com.example.isadash.uiucfit;
 
+import android.content.ContentValues;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.ContactsContract;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.OutputStream;
 
 public class camera extends AppCompatActivity {
+
+    private ImageView imgView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+
+        Button btnCamera = (Button) findViewById(R.id.buttonCam);
+
+        imgView = (ImageView) findViewById(R.id.imageViewCam);
+        btnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 0);
+            }
+        });
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+        imgView.setImageBitmap(bitmap);
+
     }
 }
+
