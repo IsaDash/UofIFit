0991d21fa94f2d519d66526eca7c7360cc6d1d0b
cbguo2 <42985049+cbguo2@users.noreply.github.com>
2018-12-11 03:54:26 -0600

    Update steps.java

--- app/src/main/java/com/example/isadash/uiucfit/steps.java
+++ app/src/main/java/com/example/isadash/uiucfit/steps.java
@@ -1,6 +1,7 @@
 package com.example.isadash.uiucfit;
 
 import android.content.Context;
+import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -13,8 +14,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class steps extends AppCompatActivity implements SensorEventListener {
-    private TextView steps;
-
+    private static TextView steps;
+    private TextView cal;
     SensorManager sensorManager;
     boolean running = false;
 
@@ -24,6 +25,16 @@ public class steps extends AppCompatActivity implements SensorEventListener {
         setContentView(R.layout.activity_steps);
 
         steps = (TextView) findViewById(R.id.textSteps);
+        //cal = (TextView) findViewById(R.id.textViewCalorie);
+//        Button btnCalories = (Button) findViewById(R.id.buttonCalorie);
+//        btnCalories.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(steps.this, Calories.class);
+//                startActivity(intent);
+//            }
+//        });
+        String stepsTaken = steps.getText().toString();
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 
     }
@@ -49,9 +60,22 @@ public class steps extends AppCompatActivity implements SensorEventListener {
 
     }
 
+    public static double getSteps() {
+        String sSteps = steps.getText().toString();sSteps = sSteps.split("$")[1].trim();
+        double wNumeric = Double.parseDouble(sSteps);
+        return wNumeric;
+    }
+
     public void onSensorChanged(SensorEvent event) {
         if (running) {
             steps.setText(String.valueOf(event.values[0]));
+//            double calories = (1.9) * profile.getWeight() * 0.45 * (event.values[0]/ 660.0);
+//            String calString = Double.toString(calories);
+//            cal = (TextView) findViewById(R.id.textViewCal);
+            //cal.setText(String.valueOf(event.values[0]));
+
+
+
         }
     }
 
