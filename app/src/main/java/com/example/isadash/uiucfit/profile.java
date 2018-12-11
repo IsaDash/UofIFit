76b18c790887c39e9adb0d36601dc50db32583e7
cbguo2 <42985049+cbguo2@users.noreply.github.com>
2018-12-10 21:02:31 -0600

    Update profile.java

--- app/src/main/java/com/example/isadash/uiucfit/profile.java
+++ app/src/main/java/com/example/isadash/uiucfit/profile.java
@@ -4,14 +4,34 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class profile extends AppCompatActivity {
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
 
-    private EditText height, weight, calorie;
+public class  profile extends AppCompatActivity {
+
+    ArrayList<String> updateHeight =  new ArrayList<String>();
+    ArrayList<String> updateWeight =  new ArrayList<String>();
+    ArrayList<String> updateCalorie =  new ArrayList<String>();
+
+    Button btnHeight;
+    Button btnWeight;
+    Button btnCalorie;
+    EditText height;
+    EditText weight;
+    EditText calorie;
+    TextView tWeight;
+    TextView tHeight;
+    TextView tCalorie;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,20 +42,50 @@ public class profile extends AppCompatActivity {
         weight = (EditText)  findViewById(R.id.editWeight);
         calorie = (EditText)  findViewById(R.id.editCalorie);
 
-        Button btnHeight = (Button) findViewById(R.id.buttonHeight);
-        Button btnWeight = (Button) findViewById(R.id.buttonWeight);
-        Button btnCalorie = (Button) findViewById(R.id.buttonCalorie);
+        btnHeight = (Button) findViewById(R.id.buttonHeight);
+        btnWeight = (Button) findViewById(R.id.buttonWeight);
+        btnCalorie = (Button) findViewById(R.id.buttonCalorie);
+        tWeight = (TextView) findViewById(R.id.textViewWeight);
+        tHeight = (TextView) findViewById(R.id.textViewHeight);
+        tCalorie = (TextView) findViewById(R.id.textViewCalorie);
+
+
         btnHeight.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String h = height.getText().toString();
+                tHeight.setText("Height: " + h);
                 Toast.makeText(profile.this, "Height updated successfully", Toast.LENGTH_LONG).show();
+                FileOutputStream saveHeight = null;
+
+                try {
+                    saveHeight = openFileOutput("UserInput.txt", MODE_PRIVATE);
+                    saveHeight.write(h.getBytes());
+
+                } catch(FileNotFoundException e) {
+                    e.printStackTrace();
+
+                } catch(IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    if (saveHeight != null) {
+                        try {
+                            saveHeight.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }
+
+
             }
         });
         btnWeight.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String w = weight.getText().toString();
+                tWeight.setText("Weight: " + w);
                 Toast.makeText(profile.this, "Weight updated successfully", Toast.LENGTH_LONG).show();
             }
         });
@@ -43,9 +93,22 @@ public class profile extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 String c = calorie.getText().toString();
+                tCalorie.setText("Calorie: " + c);
                 Toast.makeText(profile.this, "Calorie goal updated successfully", Toast.LENGTH_LONG).show();
             }
         });
 
+
+
+
+        }
+
+
+
+
+
+
+
+
     }
-}
+
